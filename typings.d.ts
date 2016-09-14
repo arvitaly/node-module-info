f0e00b6322dd8d7b669aeb7a7c72671700c57aca
Alexandro <arvitaly@list.ru>
2016-09-14 16:39:18 +0700

    fix typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,36 +1,38 @@
 declare module "node-module-info" {
-    interface Package {
-        name: string;
-        version: string;
+    function NodeModuleInfo(modulePath: string, caller?: string): NodeModuleInfo.Info;
+    namespace NodeModuleInfo {
+        interface Package {
+            name: string;
+            version: string;
+        }
+        interface Info {
+            getFullInfo: () => {
+                fullPath: string,
+                resolvedPath: string,
+                rootPath: string,
+                packagePath: string,
+                relativePath: string,
+                relativeName: string,
+                isDependence: boolean,
+                isSystem: boolean,
+                isAbsolute: boolean,
+                isRelative: boolean,
+                packageInfo: Package,
+                nearestPackageJSON: string
+            },
+            getCallerInfo: () => Info;
+            getFullPath: () => string;
+            getResolvedPath: () => string;
+            getRootPath: () => string;
+            getPackagePath: () => string;
+            getRelativePath: () => string;
+            getRelativeName: () => string;
+            isAbsoulte: () => boolean;
+            isDependence: () => boolean;
+            isRelative: () => boolean;
+            isSystem: () => boolean;
+            getPackageInfo: () => Package;
+        }
     }
-    interface Info {
-        getFullInfo: () => {
-            fullPath: string,
-            resolvedPath: string,
-            rootPath: string,
-            packagePath: string,
-            relativePath: string,
-            relativeName: string,
-            isDependence: boolean,
-            isSystem: boolean,
-            isAbsolute: boolean,
-            isRelative: boolean,
-            packageInfo: Package,
-            nearestPackageJSON: string
-        },
-        getCallerInfo: () => Info;
-        getFullPath: () => string;
-        getResolvedPath: () => string;
-        getRootPath: () => string;
-        getPackagePath: () => string;
-        getRelativePath: () => string;
-        getRelativeName: () => string;
-        isAbsoulte: () => boolean;
-        isDependence: () => boolean;
-        isRelative: () => boolean;
-        isSystem: () => boolean;
-        getPackageInfo: () => Package;
-    }
-    function getInfo(modulePath: string, caller?: string): Info;
-    export = getInfo;
+    export = NodeModuleInfo;
 }
